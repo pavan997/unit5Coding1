c4ff2f6d63af72e3b529413de81976c8987398c8
pavan <pavan_fw10_057@masai.school>
2021-10-18 16:35:09 +0530

    sixth commit

--- exam/src/Components/Grocery.jsx
+++ exam/src/Components/Grocery.jsx
@@ -1,11 +1,27 @@
 import React from 'react'
 
 
-function Grocery({}) {
+function Grocery({e,handleAdd,handleSub}) {
     
     return (
         <div>
-            
+            <img src={`${e.image_url}`} alt='image' />
+            <div>
+                <p>{e.product_name}</p>
+                <p>{e.description}</p>
+                <p>{e.price}</p>
+            </div>
+
+            <div>
+                <div>
+                    <button onClick={()=>handleSub(e.id)}>-</button>
+                    <div>{e.qty}</div>
+                    <button onClick={()=>handleAdd(e.id)}>+</button>
+                </div>
+                {e.is_available ? <button>Highstock</button>:<button>Lowstock</button>}
+
+            </div>
+                      
         </div>
     )
 }
