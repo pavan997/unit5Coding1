140ccfdd094b93061a8b8dc89d9794ffff49cb05
pavan <pavan_fw10_057@masai.school>
2021-10-18 17:21:11 +0530

    final1

--- exam/src/Components/Grocery.jsx
+++ exam/src/Components/Grocery.jsx
@@ -5,10 +5,13 @@ function Grocery({data1}) {
     
     return (
         
-        <div>
+        <div className={styles.main_1}>
+            <img src={data1.image_url} alt='img' width='120' height='120'/>
             <div className={styles.box1}>
                 <p>{data1.product_name}</p>
                 <p>{data1.description}</p>
+                <p>{data1.price}</p>
+                {data1.is_available ? <button style={{backgroundColor:'red'}}>High Stock</button>:<button style={{backgroundColor:'blue'}}>Low Stock</button>}
             </div>
 
         </div>
